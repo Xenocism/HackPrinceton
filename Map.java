0cf0906684a9462a251079d002c66c36798155db
Xenocism <samuelrf@princeton.edu>
2015-11-15 00:04:02 -0500

    Changes

--- Map.java
+++ Map.java
@@ -2,9 +2,9 @@ import java.util.LinkedList;
 
 public class Map {
 	
-	private Terrain terrMap[][]; // array of Terrain objects
-	private RedBlackBST<Integer, Actor> dirtTree;
-    private LinkedList<Actor> dirt;
+	private Terrain[][] terrMap; // array of Terrain objects
+	private RedBlackBST<Integer, Terrain> dirtTree;
+    private LinkedList<Terrain> dirt;
     private int count;
     private GameScreen screen;
 
@@ -12,13 +12,21 @@ public class Map {
 
 		this.count = 0;
 		this.screen = screen;
-		this.dirt = new LinkedList<Actor>();
-        this.dirtTree = new RedBlackBST<Integer, Actor>();
-		terrMap = new Terrain[100][100]; // hard coded number of Terrain object in map
+		this.dirt = new LinkedList<Terrain>();
+        this.dirtTree = new RedBlackBST<Integer, Terrain>();
+		this.terrMap = new Terrain[100][100]; // hard coded number of Terrain object in map
 
 		for (int i = 0; i < 100; i++) {
 			for (int j = 0; i < 100; i++) {
-				terrMap = null; 
+				terrMap[i][j] = null; 
+			}
+		}
+		screen.setTerrain(getMap());
+		int id = 0;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				place(id, i, j);
+				id++;
 			}
 		}
 	}
@@ -29,7 +37,7 @@ public class Map {
 	}
 
 	public boolean isTerrainIndex(int row, int col) {
-		return terrMap[row][col] != null;
+		return (terrMap[row][col] != null);
 	}
 
 	public int convert(int x) {
@@ -38,7 +46,7 @@ public class Map {
 
 	public void destroy(int x, int y) {
 		if (isTerrain(x, y)) {
-			int id = terrMap[convert(x)][convert(y)].getID;
+			int id = terrMap[convert(x)][convert(y)].getID();
 			dirtTree.delete(id);
 	        Iterable<Integer> keys = dirtTree.keys();
 	        dirt = new LinkedList<Terrain>();
@@ -50,11 +58,11 @@ public class Map {
 	    }
 	}
 
-	public void place(int row, int col) {
+	public void place(int id, int row, int col) {
 		if (!isTerrainIndex(row, col));
-		Terrain terr = new Terrain(row, col);
+		Terrain terr = new Terrain(id, 27 * row, 27 * col, "images\\dirt.jpg");
 		terrMap[row][col] = terr;
-		dirtTree.put(new Integer(count), terr);
+		dirtTree.put(new Integer(count), terr); 
 		dirt.push(terr);
 		count++;
 	}
